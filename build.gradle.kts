a0050e34b215fd97c518e0be4fdb048dd1104920
Stepan Sandulyak <ssandulyak@usetech.ru>
2022-11-13 23:26:07 +0300

    Android Added

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,29 @@
 import java.io.ByteArrayOutputStream;
 
+buildscript {
+    val kotlinVersion: String by project
+    val kotlinterGradle: String by project
+    val gradleVersionsPlugin: String by project
+
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+        maven(uri("https://plugins.gradle.org/m2/")) // For kotlinter-gradle
+    }
+
+    dependencies {
+        // keeping this here to allow AS to automatically update
+        classpath("com.android.tools.build:gradle:7.2.0")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:${kotlinVersion}")
+        classpath("org.jetbrains.kotlin:kotlin-serialization:${kotlinVersion}")
+        classpath("org.jmailen.gradle:kotlinter-gradle:${kotlinterGradle}")
+        classpath("com.github.ben-manes:gradle-versions-plugin:${gradleVersionsPlugin}")
+    }
+}
+
 val ktorVersion: String by project
+val composeCompiler: String by project
 val kotlinxCoroutinesVersion: String by project
 
 group = "who.we"
@@ -9,10 +32,12 @@ version = "1.0-SNAPSHOT"
 plugins {
     kotlin("multiplatform") version "1.7.20"
     kotlin("plugin.serialization") version "1.7.20"
+    id("com.android.library")
     id("maven-publish")
 }
 
 repositories {
+    google()
     mavenCentral()
     maven { url = uri("https://maven.pkg.jetbrains.space/public/p/ktor/eap") }
     maven { url = uri("https://dl.bintray.com/emerald/polkaj") }
@@ -20,7 +45,31 @@ repositories {
     mavenLocal()
 }
 
+configurations.all {
+    resolutionStrategy.dependencySubstitution {
+        substitute(module("org.jetbrains.compose.compiler:compiler")).apply {
+            using(module("androidx.compose.compiler:compiler:${composeCompiler}"))
+        }
+    }
+}
+
+android {
+    compileSdk = 33
+    sourceSets["main"].manifest.srcFile("src/androidMain/AndroidManifest.xml")
+    defaultConfig {
+        minSdk = 21
+        targetSdk = 33
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    namespace = "com.surrus.common"
+}
+
 kotlin {
+    android()
     jvm {
         compilations.all {
             kotlinOptions.jvmTarget = "1.8"
@@ -151,6 +200,16 @@ kotlin {
                 implementation(kotlin("test"))
                 implementation("ch.qos.logback:logback-classic:1.2.3")
                 implementation("io.ktor:ktor-client-apache:$ktorVersion")
+            }
+        }
+        val androidMain by getting {
+            dependencies {
+                implementation("io.ktor:ktor-client-android:$ktorVersion")
+            }
+        }
+        val androidTest by getting {
+            dependencies {
+                implementation("io.ktor:ktor-client-android:$ktorVersion")
             }
         }
         val jsMain by getting
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,2 +1,22 @@
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        mavenCentral()
+        google()
+        maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
+    }
+    resolutionStrategy {
+        eachPlugin {
+            if (requested.id.id.startsWith("com.google.cloud.tools.appengine")) {
+                useModule("com.google.cloud.tools:appengine-gradle-plugin:${requested.version}")
+            } else if (requested.id.namespace == "com.android" || requested.id.name == "kotlin-android-extensions") {
+                useModule("com.android.tools.build:gradle:7.2.0")
+            } else if (requested.id.id == "com.squareup.sqldelight"){
+                useModule("com.squareup.sqldelight:gradle-plugin:1.4.4")
+            }
+        }
+    }
+}
+
 rootProject.name = "unique-sdk-kotlin"
 
