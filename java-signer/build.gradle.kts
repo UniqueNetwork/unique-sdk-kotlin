dd9c960f91b316fd2d9c88e7bb7337cca871754e
Stepan Sandulyak <ssandulyak@usetech.ru>
2022-12-02 20:34:49 +0300

    Return pipeline to live

--- java-signer/build.gradle.kts
+++ java-signer/build.gradle.kts
@@ -31,73 +31,73 @@ if (System.getenv("SIGN_ENABLED")?.toBoolean() ?: signEnabled.toBoolean()) {
 
 val rustBasePath = project.ext.get("rust_base_path")
 
-//tasks.create("cargo-build", Exec::class.java) {
-//    dependsOn("cargo-output-dir")
-//    workingDir(rustBasePath!!)
-//    commandLine("cargo", "build", "--release")
-//}
-//
-//tasks.create("cargo-build-android-armv7", Exec::class.java) {
-//    dependsOn("cargo-output-dir")
-//    workingDir(rustBasePath!!)
-//    commandLine("cross", "build", "--target", "armv7-linux-androideabi", "--release")
-//}
-//
-//tasks.create("cargo-build-android-i686", Exec::class.java) {
-//    dependsOn("cargo-output-dir")
-//    workingDir(rustBasePath!!)
-//    commandLine("cross", "build", "--target", "i686-linux-android", "--release")
-//}
-//
-//tasks.create("cargo-build-android-aarch64", Exec::class.java) {
-//    dependsOn("cargo-output-dir")
-//    workingDir(rustBasePath!!)
-//    commandLine("cross", "build", "--target", "aarch64-linux-android", "--release")
-//}
-//
-//tasks.create("cargo-build-android-x86_64", Exec::class.java) {
-//    dependsOn("cargo-output-dir")
-//    workingDir(rustBasePath!!)
-//    commandLine("cross", "build", "--target", "x86_64-linux-android", "--release")
-//}
-//
-//tasks.create("clean-rust", Delete::class.java) {
-//    delete("${project.ext.get("cargo_target_directory")}")
-//    delete("${rustBasePath}/Cargo.lock")
-//}
-//
-//tasks["clean"].dependsOn("clean-rust")
-//tasks.withType(JavaCompile::class.java) {
-//    dependsOn("cargo-build")
-//}
-//
-//tasks.withType(Jar::class.java) {
-//    dependsOn("cargo-build-android-armv7")
-//    from("${project.ext.get("cargo_target_directory")}/armv7-linux-androideabi/release/libsigner.so") {
-//        into("lib/armeabi-v7a/")
-//    }
-//}
-//
-//tasks.withType(Jar::class.java) {
-//    dependsOn("cargo-build-android-i686")
-//    from("${project.ext.get("cargo_target_directory")}/i686-linux-android/release/libsigner.so") {
-//        into("lib/x86/")
-//    }
-//}
-//
-//tasks.withType(Jar::class.java) {
-//    dependsOn("cargo-build-android-aarch64")
-//    from("${project.ext.get("cargo_target_directory")}/aarch64-linux-android/release/libsigner.so") {
-//        into("lib/arm64-v8a/")
-//    }
-//}
-//
-//tasks.withType(Jar::class.java) {
-//    dependsOn("cargo-build-android-x86_64")
-//    from("${project.ext.get("cargo_target_directory")}/x86_64-linux-android/release/libsigner.so") {
-//        into("lib/x86_64/")
-//    }
-//}
+tasks.create("cargo-build", Exec::class.java) {
+    dependsOn("cargo-output-dir")
+    workingDir(rustBasePath!!)
+    commandLine("cargo", "build", "--release")
+}
+
+tasks.create("cargo-build-android-armv7", Exec::class.java) {
+    dependsOn("cargo-output-dir")
+    workingDir(rustBasePath!!)
+    commandLine("cross", "build", "--target", "armv7-linux-androideabi", "--release")
+}
+
+tasks.create("cargo-build-android-i686", Exec::class.java) {
+    dependsOn("cargo-output-dir")
+    workingDir(rustBasePath!!)
+    commandLine("cross", "build", "--target", "i686-linux-android", "--release")
+}
+
+tasks.create("cargo-build-android-aarch64", Exec::class.java) {
+    dependsOn("cargo-output-dir")
+    workingDir(rustBasePath!!)
+    commandLine("cross", "build", "--target", "aarch64-linux-android", "--release")
+}
+
+tasks.create("cargo-build-android-x86_64", Exec::class.java) {
+    dependsOn("cargo-output-dir")
+    workingDir(rustBasePath!!)
+    commandLine("cross", "build", "--target", "x86_64-linux-android", "--release")
+}
+
+tasks.create("clean-rust", Delete::class.java) {
+    delete("${project.ext.get("cargo_target_directory")}")
+    delete("${rustBasePath}/Cargo.lock")
+}
+
+tasks["clean"].dependsOn("clean-rust")
+tasks.withType(JavaCompile::class.java) {
+    dependsOn("cargo-build")
+}
+
+tasks.withType(Jar::class.java) {
+    dependsOn("cargo-build-android-armv7")
+    from("${project.ext.get("cargo_target_directory")}/armv7-linux-androideabi/release/libsigner.so") {
+        into("lib/armeabi-v7a/")
+    }
+}
+
+tasks.withType(Jar::class.java) {
+    dependsOn("cargo-build-android-i686")
+    from("${project.ext.get("cargo_target_directory")}/i686-linux-android/release/libsigner.so") {
+        into("lib/x86/")
+    }
+}
+
+tasks.withType(Jar::class.java) {
+    dependsOn("cargo-build-android-aarch64")
+    from("${project.ext.get("cargo_target_directory")}/aarch64-linux-android/release/libsigner.so") {
+        into("lib/arm64-v8a/")
+    }
+}
+
+tasks.withType(Jar::class.java) {
+    dependsOn("cargo-build-android-x86_64")
+    from("${project.ext.get("cargo_target_directory")}/x86_64-linux-android/release/libsigner.so") {
+        into("lib/x86_64/")
+    }
+}
 
 tasks.test {
     useJUnitPlatform()
